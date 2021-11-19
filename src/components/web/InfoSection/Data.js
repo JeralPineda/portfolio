347e8f8989a69fb32ad8457a9a4529011f9755d5
Jeral <jeral.cris@gmail.com>
2021-11-18 22:34:57 -0600

    Data para el componente infoSection

---
+++ src/components/web/InfoSection/Data.js
@@ -0,0 +1,35 @@
+// export const projects = [
+//    {
+//       id: 1,
+//       linkGitHub: '',
+//       linkDemo: '',
+//       title: '',
+//       descriptions: '',
+//       image: '',
+//    },
+// ];
+
+// export const skills = [
+//    {
+//       id: 1,
+//       icon: '',
+//       color: '',
+//    },
+// ];
+
+export const homeObjOne = {
+   id: 'about',
+   lightBg: false,
+   lightText: true,
+   lightTextDesc: true,
+   topLine: 'Premium Bank',
+   headLine: 'Unlimited Transactions with zero fees',
+   description: 'Get access to our exclusive app that allows you to send unlimited trabsactions without getting charged any fees.',
+   buttonLabel: 'Get Started',
+   imgStart: false,
+   img: 'svg-1',
+   alt: 'Computo',
+   dark: true,
+   primary: true,
+   darkText: false,
+};
