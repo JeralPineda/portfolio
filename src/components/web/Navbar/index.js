7bbda67db2745ee453535ccbc68448f423d4f984
Jeral <jeral.cris@gmail.com>
2021-11-20 01:19:43 -0600

    Funcionalidad de scroll en el navbar

--- src/components/web/Navbar/index.js
+++ src/components/web/Navbar/index.js
@@ -1,34 +1,99 @@
-import React from 'react';
-import { MobileIcon, Nav, NavbarContainer, NavItem, NavLinks, NavLogo, NavMenu } from './NavbarElements';
+import React, { useEffect, useState } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
+import { IconContext } from 'react-icons/lib';
 import { FaBars } from 'react-icons/fa';
 
+import { MobileIcon, Nav, NavbarContainer, NavItem, NavLinks, NavLogo, NavMenu } from './NavbarElements';
+
 export const Navbar = ({ toggle }) => {
+   const [scrollNav, setScrollNav] = useState(false);
+
+   const changeNav = () => {
+      if (window.scrollY >= 80) {
+         setScrollNav(true);
+      } else {
+         setScrollNav(false);
+      }
+   };
+
+   useEffect(() => {
+      window.addEventListener('scroll', changeNav);
+   }, []);
+
+   const toggleHome = () => {
+      scroll.scrollToTop();
+   };
+
    return (
       <>
-         <Nav>
-            <NavbarContainer>
-               <NavLogo to='/'>Jeral</NavLogo>
+         <IconContext.Provider value={{ color: '#fafafa' }}>
+            <Nav scrollNav={scrollNav}>
+               <NavbarContainer>
+                  <NavLogo to='/' onClick={toggleHome}>
+                     Jeral
+                  </NavLogo>
 
-               <MobileIcon onClick={toggle}>
-                  <FaBars />
-               </MobileIcon>
+                  <MobileIcon onClick={toggle}>
+                     <FaBars />
+                  </MobileIcon>
 
-               <NavMenu>
-                  <NavItem>
-                     <NavLinks to='/about'>About</NavLinks>
-                  </NavItem>
-                  <NavItem>
-                     <NavLinks to='/discover'>Discover</NavLinks>
-                  </NavItem>
-                  <NavItem>
-                     <NavLinks to='/services'>Services</NavLinks>
-                  </NavItem>
-                  <NavItem>
-                     <NavLinks to='/signup'>Sign Up</NavLinks>
-                  </NavItem>
-               </NavMenu>
-            </NavbarContainer>
-         </Nav>
+                  <NavMenu>
+                     <NavItem>
+                        <NavLinks
+                           //
+                           to='about'
+                           smooth={true}
+                           duration={500}
+                           spy={true}
+                           exact='true'
+                           offset={-80}
+                        >
+                           About
+                        </NavLinks>
+                     </NavItem>
+                     <NavItem>
+                        <NavLinks
+                           //
+                           to='discover'
+                           smooth={true}
+                           duration={500}
+                           spy={true}
+                           exact='true'
+                           offset={-80}
+                        >
+                           Discover
+                        </NavLinks>
+                     </NavItem>
+                     <NavItem>
+                        <NavLinks
+                           //
+                           to='services'
+                           smooth={true}
+                           duration={500}
+                           spy={true}
+                           exact='true'
+                           offset={-80}
+                        >
+                           Services
+                        </NavLinks>
+                     </NavItem>
+                     <NavItem>
+                        <NavLinks
+                           //
+                           to='signup'
+                           smooth={true}
+                           duration={500}
+                           spy={true}
+                           exact='true'
+                           offset={-80}
+                        >
+                           Sign Up
+                        </NavLinks>
+                     </NavItem>
+                  </NavMenu>
+               </NavbarContainer>
+            </Nav>
+         </IconContext.Provider>
       </>
    );
 };
