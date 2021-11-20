864e4b56d81c10fed99f31f0302d72349e9a51fd
Jeral <jeral.cris@gmail.com>
2021-11-20 11:04:35 -0600

    Mejorando el nabvar

--- src/components/web/Navbar/NavbarElements.js
+++ src/components/web/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import { Link as LinkR } from 'react-router-dom';
 import { Link as LinkS } from 'react-scroll';
 
 export const Nav = styled.nav`
-   /* background: ${({ scrollNav }) => (scrollNav ? '#000' : 'transparent')}; */
+   background: ${({ scrollNav }) => (scrollNav ? '#0c0c0caa' : 'transparent')};
    height: 8rem;
    margin-top: -8rem;
    display: flex;
