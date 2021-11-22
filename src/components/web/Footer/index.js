f7eedc3aee5087cd60a8f3338586b94dcea649c0
Jeral <jeral.cris@gmail.com>
2021-11-22 00:03:23 -0600

    Editando el footer

--- src/components/web/Footer/index.js
+++ src/components/web/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
 
 import { FooterContainer, FooterLink, FooterLinkItems, FooterLinksContainer, FooterLinksWrapper, FooterLinkTitle, FooterWrap, SocialIconLink, SocialIcons, SocialLogo, SocialMedia, SocialMediaWrap } from './FooterElements';
 
@@ -12,17 +12,11 @@ export const Footer = () => {
                   <FooterLinkItems>
                      <FooterLinkTitle>About Us</FooterLinkTitle>
                      <FooterLink to='/'>About us</FooterLink>
-                     <FooterLink to='/'>How it works</FooterLink>
-                     <FooterLink to='/'>Testimonials</FooterLink>
-                     <FooterLink to='/'>Careers</FooterLink>
                   </FooterLinkItems>
 
                   <FooterLinkItems>
                      <FooterLinkTitle>Videos</FooterLinkTitle>
                      <FooterLink to='/'>Submit Video</FooterLink>
-                     <FooterLink to='/'>Ambassadors</FooterLink>
-                     <FooterLink to='/'>Agency</FooterLink>
-                     <FooterLink to='/'>Influencer</FooterLink>
                   </FooterLinkItems>
                </FooterLinksWrapper>
 
@@ -30,17 +24,11 @@ export const Footer = () => {
                   <FooterLinkItems>
                      <FooterLinkTitle>Contact Us</FooterLinkTitle>
                      <FooterLink to='/'>Contact</FooterLink>
-                     <FooterLink to='/'>Support</FooterLink>
-                     <FooterLink to='/'>Destination</FooterLink>
-                     <FooterLink to='/'>Sponsorships</FooterLink>
                   </FooterLinkItems>
 
                   <FooterLinkItems>
                      <FooterLinkTitle>Social Media</FooterLinkTitle>
                      <FooterLink to='/'>Instagram</FooterLink>
-                     <FooterLink to='/'>Facebook</FooterLink>
-                     <FooterLink to='/'>Youtube</FooterLink>
-                     <FooterLink to='/'>Twitter</FooterLink>
                   </FooterLinkItems>
                </FooterLinksWrapper>
             </FooterLinksContainer>
@@ -50,19 +38,13 @@ export const Footer = () => {
                   <SocialLogo to='/'>Jeral Pineda</SocialLogo>
 
                   <SocialIcons>
-                     <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
-                        <FaFacebook />
+                     <SocialIconLink to='https://github.com/JeralPineda' target='_blank' aria-label='GitHub'>
+                        <FaGithub />
                      </SocialIconLink>
-                     <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
-                        <FaInstagram />
-                     </SocialIconLink>
-                     <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
-                        <FaYoutube />
-                     </SocialIconLink>
-                     <SocialIconLink href='https://twitter.com/jeral22800823' target='_blank' aria-label='Twitter'>
+                     <SocialIconLink to='https://twitter.com/jeral22800823' target='_blank' aria-label='Twitter'>
                         <FaTwitter />
                      </SocialIconLink>
-                     <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+                     <SocialIconLink to='https://es.linkedin.com/' target='_blank' aria-label='Linkedin'>
                         <FaLinkedinIn />
                      </SocialIconLink>
                   </SocialIcons>
