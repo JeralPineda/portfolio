f64507f4eae372cf3974b7bd665ff4d6e5b65ff1
Jeral <jeral.cris@gmail.com>
2021-11-21 23:19:52 -0600

    Agregando un BotonLink de react-router-dom

--- src/components/ButtonElement.js
+++ src/components/ButtonElement.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-scroll';
+import { Link as LinkR } from 'react-router-dom';
 
 export const Button = styled(Link)`
    border-radius: 1.1rem;
@@ -14,6 +15,30 @@ export const Button = styled(Link)`
    justify-content: center;
    align-items: center;
    transition: all 0.2s ease-out ease-out;
+
+   background: rgb(0, 0, 0);
+   background: linear-gradient(90deg, rgba(0, 0, 0, 1) 0%, rgba(16, 1, 23, 1) 0%, rgba(30, 2, 40, 1) 26%, rgba(89, 2, 113, 1) 100%);
+   border: ${({ primary }) => (primary ? '1px solid #ccc' : '1px solid #ccc')};
+
+   &:hover {
+      background: ${({ primary }) => (primary ? '#111' : '#111')};
+   }
+`;
+
+export const ButtonLink = styled(LinkR)`
+   border-radius: 1.1rem;
+   background: ${({ primary }) => (primary ? '#590271' : '#010606')};
+   white-space: nowrap;
+   padding: ${({ big }) => (big ? '14px 48px' : '12px 30px')};
+   color: ${({ dark }) => (dark ? '#fafafa' : '#fafafa')};
+   font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
+   outline: none;
+   border: none;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   transition: all 0.2s ease-out ease-out;
+   text-decoration: none;
 
    background: rgb(0, 0, 0);
    background: linear-gradient(90deg, rgba(0, 0, 0, 1) 0%, rgba(16, 1, 23, 1) 0%, rgba(30, 2, 40, 1) 26%, rgba(89, 2, 113, 1) 100%);
