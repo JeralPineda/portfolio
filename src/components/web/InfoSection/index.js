ee47e31f6f412090a7f0050d9a7260f5fdffec25
Jeral <jeral.cris@gmail.com>
2021-11-21 23:33:23 -0600

    limpiando codigo y cambiando el boton

--- src/components/web/InfoSection/index.js
+++ src/components/web/InfoSection/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Button } from 'components/ButtonElement';
+import { ButtonLink } from 'components/ButtonElement';
 import { BtnWrap, Column1, Column2, Heading, Img, ImgWrap, InfoContainer, InfoRow, InfoWrapper, Subtitle, TextWrapper, TopLine } from './InfoElements';
 
-export const InfoSection = ({ lightBg, lightText, id, imgStart, topLine, headLine, darkText, description, buttonLabel, img, alt, primary, dark, dark2 }) => {
+export const InfoSection = ({ lightBg, lightText, id, imgStart, topLine, headLine, darkText, description, buttonLabel, img, alt, primary, dark, dark2, pdf }) => {
    return (
       <>
          <InfoContainer lightBg={lightBg} id={id}>
@@ -15,20 +15,16 @@ export const InfoSection = ({ lightBg, lightText, id, imgStart, topLine, headLin
                         <Heading lightText={lightText}>{headLine}</Heading>
                         <Subtitle darkText={darkText}>{description}</Subtitle>
                         <BtnWrap>
-                           <Button
+                           <ButtonLink
                               //
-                              to='home'
-                              smooth={true}
-                              duration={500}
-                              spy={true}
-                              exact={'true'}
-                              offset={-80}
+                              to={pdf}
+                              target='_blank'
                               primary={primary ? 1 : 0}
                               dark={dark ? 1 : 0}
                               dark2={dark2 ? 1 : 0}
                            >
                               {buttonLabel}
-                           </Button>
+                           </ButtonLink>
                         </BtnWrap>
                      </TextWrapper>
                   </Column1>
