7c5de443282436e822546c50aaa747f1791ceaf4
Jeral <jeral.cris@gmail.com>
2021-11-23 17:30:05 -0600

    Cambiando la importacion de la imagen

--- src/components/web/Project/index.js
+++ src/components/web/Project/index.js
@@ -1,33 +1,17 @@
 import React from 'react';
-import { ProjectsCard, ProjectsContainer, ProjectsH1, ProjectsH2, ProjectsIcon, ProjectsP, ProjectsWrapper } from './ProjectsElements';
 
-import Icon1 from 'assets/images/svg-1.svg';
-import Icon2 from 'assets/images/svg-4.svg';
-import Icon3 from 'assets/images/svg-3.svg';
+import { CardList } from '../CardElements/CardList';
+import { ProjectsContainer, ProjectsH1, ProjectsWrapper } from './ProjectsElements';
+
+import img from 'assets/images/1.jpg';
 
 export const Projects = () => {
    return (
-      <ProjectsContainer id='projects'>
+      <ProjectsContainer id='projects' img={img}>
          <ProjectsH1>Proyectos</ProjectsH1>
 
          <ProjectsWrapper>
-            <ProjectsCard>
-               <ProjectsIcon src={Icon1} />
-               <ProjectsH2>Reduce expenses</ProjectsH2>
-               <ProjectsP>We help reduce your fess and increase your overall revenue.</ProjectsP>
-            </ProjectsCard>
-
-            <ProjectsCard>
-               <ProjectsIcon src={Icon2} />
-               <ProjectsH2>Virtual Offices</ProjectsH2>
-               <ProjectsP>We help reduce your fess and increase your overall revenue.</ProjectsP>
-            </ProjectsCard>
-
-            <ProjectsCard>
-               <ProjectsIcon src={Icon3} />
-               <ProjectsH2>Premium Benefits</ProjectsH2>
-               <ProjectsP>We help reduce your fess and increase your overall revenue.</ProjectsP>
-            </ProjectsCard>
+            <CardList />
          </ProjectsWrapper>
       </ProjectsContainer>
    );
