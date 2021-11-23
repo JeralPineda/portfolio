f6f29a0171b84e968f412e97df3419b483d3cbfe
Jeral <jeral.cris@gmail.com>
2021-11-23 17:08:41 -0600

    Data de los proyectos finalizada

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,10 +1,31 @@
+import { projectImages } from 'helpers/heroImages';
+
 export const projects = [
    {
-      id: 1,
-      linkGitHub: '',
-      linkDemo: '',
-      title: '',
-      descriptions: '',
-      image: '',
+      id: '1',
+      linkGitHub: 'https://github.com/JeralPineda',
+      linkDemo: 'https://github.com/JeralPineda',
+      title: 'Portafolio Personal',
+      subtitle: 'React y  Styled Components',
+      descriptions: 'Este es mi portafolio personal',
+      img: `${projectImages('./wallpaper.jpg').default}`,
+   },
+   {
+      id: '2',
+      linkGitHub: 'https://github.com/JeralPineda/MERN-Calendar-FrontEnd',
+      linkDemo: 'https://mern-calendar-jp.herokuapp.com',
+      title: 'MERN Calendar',
+      subtitle: 'React, Node.js, MongoDB y Redux ',
+      descriptions: 'App para crear eventos, borrar y editarlos',
+      img: `${require('assets/images/projects/wallpaper.jpg').default}`,
+   },
+   {
+      id: '3',
+      linkGitHub: 'https://github.com/JeralPineda/Modulo_Login',
+      linkDemo: 'http://app-aaf167a3-51b2-4191-b15d-b4bbf85e5262.cleverapps.io/',
+      title: 'Modulo Login',
+      subtitle: 'Node.js, MySQL, Handlebars y Bootstrap',
+      descriptions: 'Modulo login, proyecto de la universidad',
+      img: `${require('assets/images/projects/wallpaper.jpg').default}`,
    },
 ];
