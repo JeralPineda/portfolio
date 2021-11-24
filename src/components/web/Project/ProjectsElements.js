deb101604a5db33acf9cca67821abd365318aa66
Jeral <jeral.cris@gmail.com>
2021-11-23 18:44:45 -0600

    Eliminando estilos innecesarios

--- src/components/web/Project/ProjectsElements.js
+++ src/components/web/Project/ProjectsElements.js
@@ -56,32 +56,6 @@ export const ProjectsWrapper = styled.div`
    }
 `;
 
-export const ProjectsCard = styled.div`
-   background: #0101;
-   border: 1px solid #ccca;
-   display: flex;
-   flex-direction: column;
-   justify-content: flex-start;
-   align-items: center;
-   border-radius: 1rem;
-   max-height: 340px;
-   padding: 3rem;
-   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
-   transition: all 0.2s ease-in-out;
-
-   &:hover {
-      transform: scale(1.02);
-      transition: all 0.2s ease-in-out;
-      cursor: pointer;
-   }
-`;
-
-export const ProjectsIcon = styled.img`
-   height: 160px;
-   width: 160px;
-   margin-bottom: 10px;
-`;
-
 export const ProjectsH1 = styled.h1`
    font-size: 2.5rem;
    color: #fafafa;
@@ -90,16 +64,4 @@ export const ProjectsH1 = styled.h1`
    @media screen and (max-width: 480px) {
       font-size: 2rem;
    }
-`;
-
-export const ProjectsH2 = styled.h2`
-   color: #fafafa;
-   font-size: 1rem;
-   margin-bottom: 10px;
-`;
-
-export const ProjectsP = styled.p`
-   color: #fafafa;
-   font-size: 1rem;
-   text-align: center;
 `;
