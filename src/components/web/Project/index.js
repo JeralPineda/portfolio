113a1a0c0361cb04a2473707cf6a90caa7943092
Jeral <jeral.cris@gmail.com>
2021-11-24 18:10:49 -0600

    cambiando la imagen en la importacion

--- src/components/web/Project/index.js
+++ src/components/web/Project/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { CardList } from '../CardElements/CardList';
 import { ProjectsContainer, ProjectsH1, ProjectsWrapper } from './ProjectsElements';
 
-import img from 'assets/images/1.jpg';
+import img from 'assets/images/projects.jpg';
 
 export const Projects = () => {
    return (
