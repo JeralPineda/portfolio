d91226b3397869d24bd3f122a1560fb8c3bdcc6e
Jeral <jeral.cris@gmail.com>
2021-11-24 18:05:57 -0600

    Limpiando la data

--- src/components/web/InfoSection/Data.js
+++ src/components/web/InfoSection/Data.js
@@ -35,20 +35,3 @@ export const homeObjTwo = {
    primary: true,
    darkText: false,
 };
-
-export const homeObjThree = {
-   id: 'skills',
-   lightBg: true,
-   lightText: false,
-   lightTextDesc: false,
-   topLine: 'Habilidades',
-   headLine: 'Unlimited Transactions with zero fees',
-   description: 'Get access to our exclusive app that allows you to send unlimited trabsactions without getting charged any fees.',
-   buttonLabel: 'Piggybank',
-   imgStart: false,
-   img: `${pageImages('./svg-3.svg').default}`,
-   alt: 'Computo',
-   dark: false,
-   primary: false,
-   darkText: true,
-};
