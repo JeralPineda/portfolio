396016c068f9cba85939474f159a5ac2c92c7185
Jeral <jeral.cris@gmail.com>
2021-12-22 17:47:34 -0600

    agregando estilo al hacer hover en el link

--- src/components/web/Navbar/NavbarElements.js
+++ src/components/web/Navbar/NavbarElements.js
@@ -83,6 +83,11 @@ export const NavLinks = styled(LinkS)`
    padding: 0 1rem;
    height: 100%;
    cursor: pointer;
+   transition: color 0.2s ease-out;
+
+   &:hover {
+      color: #ccca;
+   }
 
    &.active {
       border-bottom: 0.3rem solid #590271;
